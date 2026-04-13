b88bc4d82976afdbc2902d957d3b7490e4250c80
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-04-13 19:14:15 +0530

    instal and update frontend project dependencies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -3,13 +3,27 @@ import jwt from "jsonwebtoken"
 import { config } from "../config/config.js";
 
 
-async function sendTokenResponse(user,res){
+async function sendTokenResponse(user,res,message){
     const token = jwt.sign({id:user._id},config.JWT_SECRET,{expiresIn:"7d"})
+  
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message,
+        success:true,
+        user:{
+            id:user._id,
+            email:user.email,
+            contact:user.contact,
+            fullname:user.fullname,
+            role:user.role
+        }
+    })
 }
 
 
-export const registerUser = async (req, res) => {
-    const { email, password, contact, fullname} = req.body;
+export const register = async (req, res) => {
+    const { email, password, contact, fullname,isSeller} = req.body;
 
     try {
         const existingUser = await userModel.findOne({
@@ -20,7 +34,9 @@ export const registerUser = async (req, res) => {
             return res.status(400).json({ message: "User with this email or contact already exists" });
         }
 
-        const user = await userModel.create({ email, password, contact, fullname });
+        const user = await userModel.create({ email, password, contact, fullname ,role:isSeller?"seller":"buyer"});
+
+        await sendTokenResponse(user,res,"User registered successfully")
 
     }catch (error) {
         console.log(error)
--- Backend/src/validator/auth.validator.js
+++ Backend/src/validator/auth.validator.js
@@ -16,6 +16,6 @@ export const validateRegisterUser = [
     body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long"),
     body("contact").notEmpty().matches(/^\d{10}$/).withMessage("Contact number must be a valid 10-digit number"),
     body("fullname").notEmpty().isLength({min:3}).withMessage("Full name must be at least 3 characters long"),
-
+    body("isSeller").isBoolean().withMessage("isSeller must be a boolean value"),
     validateRequest
 ]
